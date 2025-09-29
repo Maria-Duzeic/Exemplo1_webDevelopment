bc82a806fdd55bc0281e913079c6ffcfb07c8929
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-29 11:31:04 -0300

    Declarando javascript

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+//alert("HELLO WORLD")
+//Declaração e variáveis 
+var nome = "FIAP teste";
+console.log(typeof nome);
+
+let nome1 = "10";
+console.log(typeof nome1);
+
+const nome2= {}
+console.log(typeof nome2)
+
+let nome3=[];
+console.log(typeof nome3)
+
+let nome4=true;
+console.log(typeof nome4)
+
+
+//{}= objeto (lista)
+//[]= array - vetor - parecido com matriz - quase banco de dados(lista de objetos)
+//()= parametro - ex: (v1,v2) - (chama método)
+
+
+
+
+
